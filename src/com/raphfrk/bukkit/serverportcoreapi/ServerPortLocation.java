dd79fb35a3e03e0397d52d965cca4896cbea6ccd
Raphfrk <raphfrk@gmail.com>
2012-01-12 02:40:09 +0000

    Added license headers
    Signed-off-by: Raphfrk <raphfrk@gmail.com>

--- src/com/raphfrk/bukkit/serverportcoreapi/ServerPortLocation.java
+++ src/com/raphfrk/bukkit/serverportcoreapi/ServerPortLocation.java
@@ -1,184 +1,68 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Raphfrk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
 package com.raphfrk.bukkit.serverportcoreapi;
 
-import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Transient;
-
 import org.bukkit.Location;
-import org.bukkit.World;
 
-@Entity
-public class ServerPortLocation implements Serializable, Cloneable {
+public interface ServerPortLocation  {
 	
-	private static final long serialVersionUID = 1L;
-
-	public ServerPortLocation() {
-	}
-
-	public ServerPortLocation(String serverName, Location loc) {
-		this(
-				serverName, 
-				(loc.getWorld()==null)?null:(loc.getWorld().getName()), 
-				loc.getX(),
-				loc.getY(),
-				loc.getZ(),
-				loc.getPitch(),
-				loc.getYaw()
-				);
-	}
-
-	public ServerPortLocation(ServerPortLocation loc) {
-		this(loc.getServer(), loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
-	}
-
-	public ServerPortLocation(String server, String world, Double x, Double y, Double z, Float yaw, Float pitch) {
-		this.server = server;
-		this.world = world;
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.pitch = pitch;
-		this.yaw = yaw;
-	}
-
-	@Id
-	protected int id;
+	public String toString();
 	
-	protected String name;
-	protected String server;
-	protected String world;
-	protected Double x;
-	protected Double y;
-	protected Double z;
-	protected Float pitch;
-	protected Float yaw;
+	public void setId(int id);
 	
-	@Transient
-	public String toString() {
-		return "[" + id + ", " + name + ", " + server + ", " + world + ", " + x + ", " + y + ", " + z + ", " + pitch + ", " + yaw + "]";
-	}
+	public int getId();
 	
-	public void setId(int id) {
-		this.id = id;
-	}
+	public void setName(String name);
 	
-	public int getId() {
-		return id;
-	}
+	public String getName();
 	
-	public void setName(String name) {
-		this.name = name;
-	}
+	public void setWorld(String world);
+
+	public void setServer(String server);
+
+	public String getWorld();
+
+	public String getServer();
+
+	public void setX(Double x);
+
+	public Double getX();
+
+	public void setY(Double y);
+
+	public Double getY();
+
+	public void setZ(Double z);
+
+	public Double getZ();
+
+	public void setYaw(Float yaw);
+
+	public Float getYaw();
 	
-	public String getName() {
-		return name;
-	}
+	public void setPitch(Float pitch);
 	
-	public void setWorld(String world) {
-		this.world = world;
-	}
-
-	public void setServer(String server) {
-		this.server = server;
-	}
-
-	public String getWorld() {
-		return world;
-	}
-
-	public String getServer() {
-		return server;
-	}
-
-	public void setX(Double x) {
-		this.x = x;
-	}
-
-	public Double getX() {
-		return this.x;
-	}
-
-	public void setY(Double y) {
-		this.y = y;
-	}
-
-	public Double getY() {
-		return this.y;
-	}
-
-	public void setZ(Double z) {
-		this.z = z;
-	}
-
-	public Double getZ() {
-		return this.z;
-	}
-
-	public void setYaw(Float yaw) {
-		this.yaw = yaw;
-	}
-
-	public Float getYaw() {
-		return this.yaw;
-	}
-
-	public void setPitch(Float pitch) {
-		this.pitch = pitch;
-	}
-
-	public Float getPitch() {
-		return this.pitch;
-	}
+	public Float getPitch();
 	
-	public Location getLocation(ServerPortCoreAPI serverPortCoreAPI) {
-
-		String pluginServerName = serverPortCoreAPI.getLocalServerName();
-		if(server != null && !server.equals(pluginServerName)) {
-			return null;
-		}
-		World bukkitWorld = null;
-		if(world != null) {
-			bukkitWorld = serverPortCoreAPI.getLocalServer().getWorld(this.world);
-			if(bukkitWorld == null) {
-				return null;
-			}
-		}
-		
-		if(bukkitWorld == null) {
-			bukkitWorld = serverPortCoreAPI.getLocalServer().getWorlds().get(0);
-		}
-		
-		if(x==null || y==null || z==null) {
-			Location spawnPosition = bukkitWorld.getSpawnLocation();
-			spawnPosition.setX(spawnPosition.getX() + 0.5);
-			spawnPosition.setZ(spawnPosition.getZ() + 0.5);
-			return spawnPosition;
-		} else if(pitch == null || yaw == null) {
-			return new Location(bukkitWorld, x, y, z);
-		} else {
-			return new Location(bukkitWorld, x, y, z, yaw, pitch);
-		}
-	}
-
-	public static ServerPortLocation fromLocation(String server, Location loc) {
-		return new ServerPortLocation(server, loc);
-	}
-
-	public static ServerPortLocation fromLocation(ServerPortCoreAPI serverPortCoreAPI, Location loc) {
-		
-		String server = serverPortCoreAPI.getLocalServerName();
-		return fromLocation(server, loc);
-	}
-	
-	@Override
-	public ServerPortLocation clone() {
-		try {
-			return (ServerPortLocation)super.clone();
-		} catch (CloneNotSupportedException e) {
-			return null;
-		}
-	}
+	public Location getLocation(ServerPortCoreAPI serverPortCoreAPI);
 	
 }
